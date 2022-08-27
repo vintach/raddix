38a268dd398bdb7cea58a3963143ec75ef7788da
immois <rmoimv@gmail.com>
2022-08-26 20:19:38 -0500

    chore(scripts): create function for create files

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -8,3 +8,11 @@ export const createFolder = (route: string, name: string) => {
     console.log('Failed to creation folder: ', e);
   }
 };
+
+export const createFile = (route: string, name: string, content: string) => {
+  try {
+    fs.writeFileSync(path.resolve(route, name), content);
+  } catch (e) {
+    console.log('Failed to creation file: ', e);
+  }
+};
