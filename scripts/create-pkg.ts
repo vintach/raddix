686921de1afc4981a9ef672b2fda65d82489d2b4
immois <rmoimv@gmail.com>
2022-08-26 20:38:31 -0500

    chore(scripts): function that creates the structure of a component package

---
+++ scripts/create-pkg.ts
@@ -0,0 +1,28 @@
+import { textOfPackage } from './text-package';
+import { textOfTsConfig } from './text-tsconfig';
+import { createFolder, createFile } from './utils';
+
+const createPackage = () => {
+  const pathMain: string = 'packages/component';
+
+  // create folder main
+  createFolder('packages', 'component');
+
+  // create package.json
+  createFile(pathMain, 'package.json', textOfPackage('component'));
+
+  // create tsconfig.json
+  createFile(pathMain, 'tsconfig.json', textOfTsConfig());
+
+  // create README
+  createFile(pathMain, 'README.md', '# react-component');
+
+  // create folders of work
+  createFolder(pathMain, 'src');
+  createFolder(pathMain, 'stories');
+  createFolder(pathMain, 'tests');
+
+  console.log('✅ Successful creation');
+};
+
+createPackage();
