1bb1138562993510441791999280fc2e5c7fd421
immois <rmoimv@gmail.com>
2022-09-02 11:03:25 -0500

    fix: tsconfig alias path

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,6 @@
-const path = require('path');
-const { lstatSync, readdirSync } = require('fs');
-
-const basePath = path.resolve(__dirname, 'packages');
-const packages = readdirSync(basePath).filter(name =>
-  lstatSync(path.join(basePath, name)).isDirectory()
-);
-
 module.exports = {
   testEnvironment: 'jsdom',
+  roots: ['<rootDir>/packages'],
   testMatch: ['**/?(*.)+(test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
@@ -17,8 +10,6 @@ module.exports = {
     'jest-watch-typeahead/testname'
   ],
   moduleNameMapper: {
-    ...packages.reduce((acc, name) => ({
-      [`@mark-ui/(.+)$`]: `<rootDir>/packages/$1/tests`
-    }))
+    '@mark-ui/(.+)$': '<rootDir>/packages/$1/src'
   }
 };
