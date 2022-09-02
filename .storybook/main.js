79a143213abfa1a890891865b34da49c7f8e7e4e
immois <rmoimv@gmail.com>
2022-09-02 11:07:00 -0500

    chore(storybook): setup tsconfig path to webpack alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,17 @@
+const path = require('path');
+
+// convert ts config paths to webpack aliases
+const convertTsConfigPathsToWebpackAliases = () => {
+  const rootDir = path.resolve(__dirname, '../');
+  const tsConfig = require('../tsconfig.json');
+  const tsConfigPaths = Object.entries(tsConfig.compilerOptions.paths);
+
+  return tsConfigPaths.reduce((alias, [realPath, mappedPath]) => {
+    alias[realPath] = path.join(rootDir, mappedPath[0]);
+    return alias;
+  }, {});
+};
+
 module.exports = {
   stories: [
     '../packages/**/*.stories.mdx',
@@ -8,5 +22,17 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions'
   ],
-  framework: '@storybook/react'
+  framework: '@storybook/react',
+
+  // add alias path to webpack
+  webpackFinal: async config => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        ...convertTsConfigPathsToWebpackAliases()
+      }
+    }
+  })
 };
