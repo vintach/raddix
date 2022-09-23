79c20dd83609ea8e00bfe92e14f7fc06b229da9a
immois <rmoimv@gmail.com>
2022-09-23 18:54:33 -0500

    feat: create create type polymorphic for custom hook of component

---
+++ packages/types/polymorphic/src/hook.ts
@@ -0,0 +1,32 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react';
+
+type ElementTypeProp<E extends React.ElementType> = {
+  elementType?: E;
+};
+
+// Props key to omit
+type PropsToOmit<E extends ElementType, Props> = keyof (ElementTypeProp<E> &
+  Props);
+
+export type PolymorphicHookProp<E extends ElementType, Props = {}> = Omit<
+  ComponentPropsWithoutRef<E>,
+  PropsToOmit<E, Props>
+> &
+  ElementTypeProp<E>;
+
+// This will return HTML props
+type NativeHTMLProps<E extends ElementType, Props> = Omit<
+  PolymorphicHookProp<E, Props>,
+  PropsToOmit<E, Props>
+>;
+
+export type HookResponse<E extends ElementType, Props = {}> = {
+  elementProps: NativeHTMLProps<E, Props>;
+};
+
+// Type that ensures hooks behave well polymorphically
+export type PolymorphicHook<D extends ElementType, Props = {}> = <
+  E extends ElementType = D
+>(
+  props: PolymorphicHookProp<E, Props>
+) => HookResponse<E, Props>;
---
+++ packages/types/polymorphic/src/index.ts
@@ -0,0 +1,5 @@
+export type {
+  HookResponse,
+  PolymorphicHook,
+  PolymorphicHookProp
+} from './hook';
