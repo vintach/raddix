08b9bfbccc5c45cb301763ba6625536eff767361
immois <rmoimv@gmail.com>
2022-09-23 20:48:26 -0500

    refactor: change types for polymorphicHook

---
+++ packages/components/button/src/button.hooks.ts
@@ -0,0 +1,84 @@
+import { useHover, HoverProps } from '@mark-hooks/usehover';
+import { usePress, PressProps } from '@mark-hooks/usepress';
+import { useMemo } from 'react';
+import { PolymorphicHook } from '@mark-types/polymorphic';
+
+type ButtonHookProps = {
+  isDisabled?: boolean;
+  disabled?: boolean;
+} & Omit<HoverProps, 'disabled'> &
+  Omit<PressProps, 'disabled'>;
+
+export type ButtonHook = PolymorphicHook<'button', ButtonHookProps>;
+
+export const useButton: ButtonHook = props => {
+  const {
+    // isDisabled: its value corresponds to the disabled attribute
+    isDisabled,
+    // disabledProps: its value corresponds to the aria-disabled attribute
+    disabled: disabledProp,
+    elementType = 'button',
+    onPress,
+    onPressEnd,
+    onPressStart,
+    onHover,
+    onHoverEnd,
+    onHoverStart,
+    ...rest
+  } = props;
+
+  const disabled = isDisabled ?? disabledProp;
+
+  const { pressEvents, isPressed } = usePress({
+    disabled,
+    onPress,
+    onPressStart,
+    onPressEnd
+  });
+  const { hoverEvents, isHovered } = useHover({
+    disabled,
+    onHover,
+    onHoverStart,
+    onHoverEnd
+  });
+
+  // current state of the component
+  const getState = useMemo(() => {
+    if (isPressed) {
+      return 'pressed';
+    }
+
+    if (isHovered) {
+      return 'hovered';
+    }
+
+    return 'ready';
+  }, [isPressed, isHovered]);
+
+  // default props depending on the element type
+  let elementTypeProps;
+  if (elementType === 'button' || elementType === 'input') {
+    elementTypeProps = {
+      type: 'button',
+      disabled: isDisabled
+    };
+  } else {
+    elementTypeProps = {
+      role: 'button'
+    };
+  }
+
+  const buttonProps = {
+    ...elementTypeProps,
+    ...hoverEvents,
+    ...pressEvents,
+    'aria-disabled': disabledProp,
+    'data-disabled': disabled,
+    'data-state': getState,
+    ...rest
+  };
+
+  return {
+    elementProps: buttonProps
+  };
+};
