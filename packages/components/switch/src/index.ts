49e1ce0247fa2d4edd8ee93f9cc5aca42c2f6ed3
immois <rmoimv@gmail.com>
2022-09-28 12:22:53 -0500

    feat: add prop isDisabled in provider

--- packages/components/switch/src/index.ts
+++ packages/components/switch/src/index.ts
@@ -1,2 +1,3 @@
 export { default, useSwitchRoot, useSwitchThumb } from './switch';
 export type { SwitchRootHookProps } from './switch';
+export { useSwitchState, SwitchProvider } from './switch.provider';
--- packages/components/switch/src/switch.provider.tsx
+++ packages/components/switch/src/switch.provider.tsx
@@ -1,23 +1,25 @@
-import React, {
+import {
   createContext,
   FC,
   PropsWithChildren,
   useContext,
   useMemo
 } from 'react';
-import { Props } from './switch.types';
+import { Props } from './switch';
 
 export const SwitchContext = createContext<Props>({
-  checked: false,
-  disabled: false
+  checked: false
 });
 
-export const useSwitch = () => useContext(SwitchContext);
+export const useSwitchState = () => useContext(SwitchContext);
 
 export const SwitchProvider: FC<PropsWithChildren<Props>> = props => {
-  const { checked, disabled, children } = props;
+  const { checked, disabled, isDisabled, children } = props;
 
-  const config = useMemo(() => ({ checked, disabled }), [checked, disabled]);
+  const config = useMemo(
+    () => ({ checked, disabled, isDisabled }),
+    [checked, disabled, isDisabled]
+  );
 
   return (
     <SwitchContext.Provider value={config}>{children}</SwitchContext.Provider>
