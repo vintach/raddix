02d2e16e7c0f9f81d15602952d0c6b1b86f45047
immois <rmoimv@gmail.com>
2022-09-29 23:28:59 -0500

    feat: new stories of switch

--- packages/components/switch/stories/switch.stories.tsx
+++ packages/components/switch/stories/switch.stories.tsx
@@ -1,64 +1,66 @@
-import React, { useState } from 'react';
-import Switch from '@mark-ui/switch';
+import { ComponentPropsWithoutRef, useState } from 'react';
+import useSwitch, { SwitchRootHookProps } from '@mark-ui/switch';
 import './switch.css';
+
+interface SwitchProps
+  extends ComponentPropsWithoutRef<'button'>,
+    SwitchRootHookProps {}
 
 export default {
   title: 'Components/Switch'
 };
 
-export const Default = () => (
-  <Switch.Root className='switch'>
-    <Switch.Thumb className='switch-thumb' />
-  </Switch.Root>
-);
+export const Switch = (props: SwitchProps) => {
+  const { elementProps, state } = useSwitch.Root(props);
+  const { elementProps: switchThumbProps } = useSwitch.Thumb(state);
 
-export const DefaultChecked = () => {
   return (
-    <Switch.Root className='switch' defaultChecked={true}>
-      <Switch.Thumb className='switch-thumb' />
-    </Switch.Root>
+    <button className='switch' {...elementProps}>
+      <span className='switch-thumb' {...switchThumbProps}></span>
+    </button>
   );
 };
 
-export const Controlled = () => {
+export const Usage = () => {
   const [checked, setChecked] = useState<boolean>(true);
 
   return (
-    <Switch.Root className='switch' checked={checked} onChecked={setChecked}>
-      <Switch.Thumb className='switch-thumb' />
-    </Switch.Root>
-  );
-};
-
-export const Disabled = () => {
-  return (
     <>
-      <Switch.Root className='switch' disabled>
-        <Switch.Thumb className='switch-thumb' />
-      </Switch.Root>
-    </>
-  );
-};
+      <h2>Default Value</h2>
+      <p>
+        Switches are not selected by default. To be checked by default using{' '}
+        <code>defaultChecked</code> prop
+      </p>
+      <Switch defaultChecked />
 
-export const ReadOnly = () => {
-  return (
-    <>
-      <Switch.Root className='switch' defaultChecked readOnly>
-        <Switch.Thumb className='switch-thumb' />
-      </Switch.Root>
-    </>
-  );
-};
+      <h2>Controlled value</h2>
+      <p>
+        To control the state, use <code>checked</code> property for initial
+        value. The <code>onChecked</code> event is fired when the{' '}
+        <code>checked</code> prop receives the new value.
+      </p>
+      <Switch checked={checked} onChecked={setChecked} />
 
-export const Label = () => {
-  return (
-    <div className='flex'>
-      <label htmlFor='dev' className='label-switch'>
-        Label
-      </label>
-      <Switch.Root className='switch' id='dev'>
-        <Switch.Thumb className='switch-thumb' />
-      </Switch.Root>
-    </div>
+      <h2>Disabled</h2>
+      <p>
+        The <code>disabled</code> prop makes switch un-clickable
+      </p>
+      <Switch disabled />
+
+      <h2>Read only</h2>
+      <p>
+        The <code>readOnly</code> prop makes the selection immutable.
+      </p>
+      <Switch defaultChecked readOnly />
+
+      <h2>Label forms</h2>
+      <p>Switch supports the integration with HTML tag Label</p>
+      <div className='flex'>
+        <label htmlFor='dev' className='label-switch'>
+          Label
+        </label>
+        <Switch id='dev' />
+      </div>
+    </>
   );
 };
