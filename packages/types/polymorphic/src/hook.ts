aca7bede5887f7eb0359916f03e938bd68112084
immois <rmoimv@gmail.com>
2022-10-03 16:28:37 -0500

    feat: props that are included inside the elementProps

--- packages/types/polymorphic/src/hook.ts
+++ packages/types/polymorphic/src/hook.ts
@@ -16,18 +16,25 @@ export type PolymorphicHookProp<E extends ElementType, Props = {}> = Omit<
   Props;
 
 // This will return HTML props
-type NativeHTMLProps<E extends ElementType, Props> = Omit<
-  PolymorphicHookProp<E, Props>,
-  PropsToOmit<E, Props>
->;
+type ElementProps<E extends ElementType, EProps = {}> = Omit<
+  ComponentPropsWithoutRef<E>,
+  keyof EProps
+> &
+  EProps;
 
-export type HookResponse<E extends ElementType, Props = {}, Rpta = {}> = {
-  elementProps: NativeHTMLProps<E, Props>;
+export type HookResponse<E extends ElementType, Rpta = {}, EProps = {}> = {
+  elementProps: ElementProps<E, EProps>;
 } & Rpta;
 
 // Type that ensures hooks behave well polymorphically
-export type PolymorphicHook<D extends ElementType, Props = {}, Rpta = {}> = <
-  E extends ElementType = D
->(
+export type PolymorphicHook<
+  D extends ElementType,
+  // Props for the hook
+  Props = {},
+  // Props that are included outside the elementProps
+  Rpta = {},
+  // Props that are included inside the elementProps
+  EProps = {}
+> = <E extends ElementType = D>(
   props: PolymorphicHookProp<E, Props>
-) => HookResponse<E, Props, Rpta>;
+) => HookResponse<E, Rpta, EProps>;
