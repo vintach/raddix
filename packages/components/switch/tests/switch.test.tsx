43bc0cefca64f6225777bbc723a7b96e3d39f278
immois <rmoimv@gmail.com>
2022-10-03 23:58:29 -0500

    test: indicates that the element is disabled

--- packages/components/switch/tests/switch.test.tsx
+++ packages/components/switch/tests/switch.test.tsx
@@ -31,4 +31,24 @@ describe('useSwitch tests', () => {
 
     expect(resultThumb.current.elementProps['data-state']).toBe('unchecked');
   });
+
+  it('indicates that the element is disabled', () => {
+    const { result } = renderHook(() =>
+      useSwitchRoot({
+        disabled: true
+      })
+    );
+    const { result: resultThumb } = renderHook(() =>
+      useSwitchThumb(result.current.state)
+    );
+
+    expect(result.current.elementProps['role']).toBe('switch');
+    expect(result.current.elementProps['aria-checked']).toBe(false);
+    expect(result.current.elementProps['data-state']).toBe('unchecked');
+    expect(result.current.elementProps['aria-disabled']).toBe(true);
+    expect(result.current.elementProps['data-disabled']).toBe(true);
+
+    expect(resultThumb.current.elementProps['data-state']).toBe('unchecked');
+    expect(resultThumb.current.elementProps['data-disabled']).toBe(true);
+  });
 });
