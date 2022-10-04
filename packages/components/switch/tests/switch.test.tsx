37e9150d1b09e2604ab05df91245d90bf6ae9c39
immois <rmoimv@gmail.com>
2022-10-03 23:52:42 -0500

    feat: change component test to hook test

--- packages/components/switch/tests/switch.test.tsx
+++ packages/components/switch/tests/switch.test.tsx
@@ -1,106 +1,34 @@
-import React, { useState } from 'react';
-import '@testing-library/jest-dom';
-import { fireEvent, render } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
+import { useSwitchRoot, useSwitchThumb } from '@mark-ui/switch';
 
-import Switch from '@mark-ui/switch';
-
-test('Uncontrolled - should check and uncheck', async () => {
-  const { container } = render(
-    <Switch.Root>
-      <Switch.Thumb />
-    </Switch.Root>
-  );
-  const button = container.querySelector('button') as HTMLButtonElement;
-
-  fireEvent.click(button);
-  expect(button).toHaveAttribute('aria-checked', 'true');
-
-  fireEvent.click(button);
-  expect(button).toHaveAttribute('aria-checked', 'false');
-});
-
-test('Default Checked - should check true', async () => {
-  const { container } = render(
-    <Switch.Root defaultChecked>
-      <Switch.Thumb />
-    </Switch.Root>
-  );
-  const button = container.querySelector('button') as HTMLButtonElement;
-
-  expect(button).toHaveAttribute('aria-checked', 'true');
-});
-
-test('Controlled - should check and uncheck', async () => {
-  const ControlledSwitch = () => {
-    const [checked, setChecked] = useState<boolean>(false);
-    return (
-      <Switch.Root checked={checked} onChecked={setChecked}>
-        <Switch.Thumb />
-      </Switch.Root>
+describe('useSwitch tests', () => {
+  it('default values', () => {
+    const { result } = renderHook(() => useSwitchRoot({}));
+    const { result: resultThumb } = renderHook(() =>
+      useSwitchThumb(result.current.state)
     );
-  };
 
-  const { container } = render(<ControlledSwitch />);
-  const button = container.querySelector('button') as HTMLButtonElement;
+    expect(result.current.elementProps['role']).toBe('switch');
+    expect(result.current.elementProps['aria-checked']).toBe(false);
+    expect(result.current.elementProps['data-state']).toBe('unchecked');
 
-  fireEvent.click(button);
-  expect(button).toHaveAttribute('aria-checked', 'true');
+    expect(resultThumb.current.elementProps['data-state']).toBe('unchecked');
+  });
 
-  fireEvent.click(button);
-  expect(button).toHaveAttribute('aria-checked', 'false');
-});
+  it('values others than button element', () => {
+    const { result } = renderHook(() =>
+      useSwitchRoot({
+        elementType: 'div'
+      })
+    );
+    const { result: resultThumb } = renderHook(() =>
+      useSwitchThumb(result.current.state)
+    );
 
-test('Disabled - indicates that the element is disabled', () => {
-  const { container } = render(
-    <Switch.Root disabled>
-      <Switch.Thumb />
-    </Switch.Root>
-  );
-  const button = container.querySelector('button') as HTMLButtonElement;
+    expect(result.current.elementProps['role']).toBe('switch');
+    expect(result.current.elementProps['aria-checked']).toBe(false);
+    expect(result.current.elementProps['data-state']).toBe('unchecked');
 
-  expect(button).toHaveAttribute('aria-checked', 'false');
-
-  fireEvent.click(button);
-  expect(button).toHaveAttribute('aria-checked', 'false');
-
-  expect(button).toHaveAttribute('aria-disabled', 'true');
-});
-
-test('ReadOnly - indicates that the element is immutable', () => {
-  const { container } = render(
-    <Switch.Root readOnly>
-      <Switch.Thumb />
-    </Switch.Root>
-  );
-
-  const button = container.querySelector('button') as HTMLButtonElement;
-
-  expect(button).toHaveAttribute('aria-checked', 'false');
-
-  fireEvent.click(button);
-  expect(button).toHaveAttribute('aria-checked', 'false');
-
-  expect(button).toHaveAttribute('aria-readonly', 'true');
-});
-
-test('Label - you must check and uncheck by clicking on label', () => {
-  const LabelSwitch = () => (
-    <div>
-      <label htmlFor='dev'>Label</label>
-      <Switch.Root id='dev'>
-        <Switch.Thumb />
-      </Switch.Root>
-    </div>
-  );
-
-  const { container } = render(<LabelSwitch />);
-
-  const label = container.querySelector('label') as HTMLLabelElement;
-  const button = container.querySelector('button') as HTMLButtonElement;
-
-  fireEvent.click(label);
-  expect(button).toHaveAttribute('aria-checked', 'true');
-
-  fireEvent.click(label);
-  expect(button).toHaveAttribute('aria-checked', 'false');
+    expect(resultThumb.current.elementProps['data-state']).toBe('unchecked');
+  });
 });
