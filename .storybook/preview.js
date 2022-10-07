9db7a684a16e00004c16678137da50a9519dedb2
immois <rmoimv@gmail.com>
2022-10-07 17:18:22 -0500

    chore(storybook): MDX component overrides

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
+import { SubTitle, Text, Title } from './src';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
+      date: /Date$/
+    }
   },
-}
+  docs: {
+    components: {
+      h1: Title,
+      h2: SubTitle,
+      p: Text
+    }
+  },
+  viewMode: 'docs'
+};
