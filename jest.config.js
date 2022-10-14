63d1f8fa5724f5aaf6f01202da7a276a67fe6953
immois <rmoimv@gmail.com>
2022-10-14 00:33:01 -0500

    chore: rename package in test config

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,6 @@ module.exports = {
     'jest-watch-typeahead/testname'
   ],
   moduleNameMapper: {
-    '@mark-ui/(.+)$': '<rootDir>/packages/components/$1/src'
+    '@raddix/(.+)$': '<rootDir>/packages/components/$1/src'
   }
 };
