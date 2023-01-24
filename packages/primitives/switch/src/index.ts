0615ae753a4ad1ae699a1e013f7c90087adef384
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-01-24 15:45:16 -0500

    feat(switch): export TS types

--- packages/primitives/switch/src/index.ts
+++ packages/primitives/switch/src/index.ts
@@ -1,2 +1,3 @@
 export { default, useSwitchRoot, useSwitchThumb } from './switch';
 export { useSwitchState, SwitchProvider } from './switch.provider';
+export type { SwitchRootBase, SwitchState } from './switch';
