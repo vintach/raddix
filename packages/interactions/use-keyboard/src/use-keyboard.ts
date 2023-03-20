c9a8d52e126ed6fb97dd15781e97c8330b5dbcb5
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-03-20 16:25:53 -0500

    feat(useKeyboard): add configuration options

--- packages/interactions/use-keyboard/src/use-keyboard.ts
+++ packages/interactions/use-keyboard/src/use-keyboard.ts
@@ -5,20 +5,42 @@ type KeyboardHandler<T extends HTMLElement = HTMLElement> = (
   event: KeyboardEvent<T>
 ) => void;
 
+interface Options {
+  stopPropagation?: boolean;
+  preventDefault?: boolean;
+  checker?: 'key' | 'code';
+}
+
 export const useKeyboard = <E extends HTMLElement = HTMLElement>(
   handler: KeyboardHandler<E>,
-  shortcut?: Keys[]
+  shortcut?: Keys[],
+  options?: Options
 ): KeyboardHandler<E> => {
-  const handleKeyboard = (event: KeyboardEvent<E>) => {
+  options = {};
+  const {
+    preventDefault = false,
+    stopPropagation = true,
+    checker = 'key'
+  } = options;
+
+  const eventHandler = (event: KeyboardEvent<E>) => {
+    if (preventDefault) {
+      event.preventDefault();
+    }
+
+    if (stopPropagation) {
+      event.stopPropagation();
+    }
+
     if (!shortcut || shortcut.length === 0) {
       handler(event);
       return;
     }
 
-    if (shortcut.includes(event.key)) {
+    if (shortcut.includes(event[checker])) {
       handler(event);
     }
   };
 
-  return handleKeyboard;
+  return eventHandler;
 };
