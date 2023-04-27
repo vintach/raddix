d097b64c28daee0ba99d601dac4d4341b69bd90d
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-04-26 22:17:26 -0500

    chore: remove useCheckbox hook

--- packages/primitives/checkbox/src/checkboxv2.types.ts
+++
@@ -1,6 +0,0 @@
-import type { InputHTMLAttributes } from 'react';
-
-export type CheckboxInputAttributes = Omit<
-  InputHTMLAttributes<HTMLInputElement>,
-  'type' | 'value' | 'defaultValue'
-> & { className?: string; onClick?: () => void; onChange?: () => void };
