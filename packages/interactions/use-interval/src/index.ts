332fc21f69ade35a28ae12b7804008507492a8d1
Moises Machuca Valverde <rmoimv@gmail.com>
2023-10-04 23:01:03 -0500

    feat: add useInterval hook

--- packages/interactions/use-interval/src/index.ts
+++ packages/interactions/use-interval/src/index.ts
@@ -1,21 +1,43 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
-type UseInterval = (callback: () => void, delay: number | null) => void;
+export interface IntervalResult {
+  /** Clear interval and stop the execution  */
+  clear: () => void;
+  /** Execute the interval */
+  run: () => void;
+}
 
-export const useInterval: UseInterval = (callback, delay) => {
-  const savedCallback = useRef<() => void>();
+type UseInterval = (
+  callback: () => void,
+  delay: number,
+  inmediate?: boolean
+) => IntervalResult;
+
+export const useInterval: UseInterval = (callback, delay, inmediate = true) => {
+  const savedCallback = useRef(callback);
+  const id = useRef<NodeJS.Timeout | null>(null);
+
+  const clearId = () => {
+    if (!id.current) return;
+    clearInterval(id.current);
+    id.current = null;
+  };
+
+  const run = useCallback(() => {
+    if (id.current) return;
+    const tick = () => savedCallback.current();
+
+    id.current = setInterval(tick, delay);
+  }, [delay]);
 
   useEffect(() => {
     savedCallback.current = callback;
-  });
+  }, [callback]);
 
   useEffect(() => {
-    if (delay === null) return;
-    const tick = () => {
-      savedCallback.current?.();
-    };
+    if (inmediate) run();
+    return () => clearId();
+  }, [run, inmediate]);
 
-    const id = setInterval(tick, delay);
-    return () => clearInterval(id);
-  }, [delay]);
+  return { clear: clearId, run };
 };
