70010d054a7b8d33cf401535a3052cd2a1f7986d
Moisés Machuca Valverde <rmoimv@gmail.com>
2023-10-18 22:50:45 -0500

    feat(useTimeout): API improved (#68)
    Added two new return functions: `reset` and `run`, added a new `immediate` option and added null type on `delay` option.

---
+++ .changeset/fluffy-waves-design.md
@@ -0,0 +1,5 @@
+---
+'@raddix/use-timeout': minor
+---
+
+Added two new return functions: `reset` and `run`
--- packages/hooks/use-timeout/tests/use-timeout.test.ts
+++ packages/hooks/use-timeout/tests/use-timeout.test.ts
@@ -46,14 +46,57 @@ describe('useTimeout test:', () => {
     expect(callback).toHaveBeenCalledTimes(1);
   });
 
+  it('should not execute the callback if the delay is null', () => {
+    const callback = jest.fn();
+    renderHook(() => useTimeout(callback, null));
+
+    jest.advanceTimersByTime(2000);
+    expect(callback).not.toBeCalled();
+  });
+
+  it('should not execute the callback immediately if the inmediate option is false', () => {
+    const callback = jest.fn();
+    renderHook(() => useTimeout(callback, 1000, false));
+
+    jest.advanceTimersByTime(2000);
+    expect(callback).not.toBeCalled();
+  });
+
   it('should stop the timeout when the clear function is called', () => {
     const callback = jest.fn();
     const { result } = renderHook(() => useTimeout(callback, 4000));
 
     expect(callback).not.toBeCalled();
     jest.advanceTimersByTime(2000);
-    result.current();
+    result.current.clear();
     jest.advanceTimersByTime(2000);
     expect(callback).not.toBeCalled();
+  });
+
+  it('should restart the timeout when the reset function is called', () => {
+    const callback = jest.fn();
+    const { result } = renderHook(() => useTimeout(callback, 4000));
+
+    expect(callback).not.toBeCalled();
+    jest.advanceTimersByTime(2000);
+    result.current.reset();
+    jest.advanceTimersByTime(2000);
+    expect(callback).not.toBeCalled();
+    jest.advanceTimersByTime(2000);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should execute the callback if the run function is called', () => {
+    const callback = jest.fn();
+    const { result } = renderHook(() => useTimeout(callback, 2000, false));
+
+    jest.advanceTimersByTime(2000);
+    expect(callback).not.toBeCalled();
+    result.current.run();
+    jest.advanceTimersByTime(4000);
+    expect(callback).toHaveBeenCalledTimes(1);
+    result.current.run();
+    jest.advanceTimersByTime(2000);
+    expect(callback).toHaveBeenCalledTimes(1);
   });
 });
