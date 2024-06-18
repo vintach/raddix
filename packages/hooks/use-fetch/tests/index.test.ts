8412955f714c4eecd8229d55ea0283feacebe83b
Moises Machuca Valverde <rmoimv@gmail.com>
2024-06-17 22:40:54 -0500

    chore: initial setup

---
+++ packages/hooks/use-fetch/tests/index.test.ts
@@ -0,0 +1,8 @@
+import { renderHook, act } from '@testing-library/react';
+import { useFetch } from '../src';
+
+describe('useFetch test:', () => {
+  it('should be defined', () => {
+    expect(0).toBe(0);
+  });
+});
