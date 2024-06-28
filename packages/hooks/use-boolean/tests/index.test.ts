3a4aedd2e3bfdc34799d8288c5bc2884ca1c2ad6
Moises Machuca Valverde <rmoimv@gmail.com>
2024-06-27 23:24:09 -0500

    feat: add tests

--- packages/hooks/use-boolean/tests/index.test.ts
+++ packages/hooks/use-boolean/tests/index.test.ts
@@ -2,7 +2,39 @@ import { renderHook, act } from '@testing-library/react';
 import { useBoolean } from '../src';
 
 describe('useBoolean test:', () => {
-  it('should be defined', () => {
-    expect(0).toBe(0);
+  it('should print the default value', () => {
+    const { result } = renderHook(() => useBoolean());
+
+    expect(result.current[0]).toBe(false);
+  });
+
+  it('should set the boolean value to true', () => {
+    const { result } = renderHook(() => useBoolean(false));
+
+    act(() => result.current[1].on());
+    expect(result.current[0]).toBe(true);
+
+    act(() => result.current[1].on());
+    expect(result.current[0]).toBe(true);
+  });
+
+  it('should set the boolean value to false', () => {
+    const { result } = renderHook(() => useBoolean(true));
+
+    act(() => result.current[1].off());
+    expect(result.current[0]).toBe(false);
+
+    act(() => result.current[1].off());
+    expect(result.current[0]).toBe(false);
+  });
+
+  it('Should toggle the boolean state.', () => {
+    const { result } = renderHook(() => useBoolean());
+
+    act(() => result.current[1].toggle());
+    expect(result.current[0]).toBe(true);
+
+    act(() => result.current[1].toggle());
+    expect(result.current[0]).toBe(false);
   });
 });
