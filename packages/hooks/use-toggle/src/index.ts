fdc1abbfbb2e7b82acaeb514480f60ba8b3c877e
Moises Machuca Valverde <rmoimv@gmail.com>
2024-07-02 18:55:02 -0500

    feat: update the useToggle API

--- packages/hooks/use-toggle/src/index.ts
+++ packages/hooks/use-toggle/src/index.ts
@@ -1,1 +1,17 @@
-export { useToggle } from './use-toggle';
+import { useState, useCallback } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
+type ReturnValue<T> = [T, () => void, Dispatch<SetStateAction<T>>];
+
+export const useToggle = <T>(options: T[], initValue?: T): ReturnValue<T> => {
+  const [value, setValue] = useState(initValue ?? options[0]);
+
+  const toggle = useCallback(() => {
+    setValue(prev => {
+      const currentIndex = options.indexOf(prev);
+      const nextIndex = (currentIndex + 1) % options.length;
+      return options[nextIndex];
+    });
+  }, [options]);
+
+  return [value, toggle, setValue];
+};
