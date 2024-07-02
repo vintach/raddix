790a9c9eb4ee94896295b8c761cd1cd0ae457f9f
Moises Machuca Valverde <rmoimv@gmail.com>
2024-07-02 18:56:01 -0500

    feat: update tests

--- packages/hooks/use-toggle/tests/use-toggle.test.ts
+++ packages/hooks/use-toggle/tests/use-toggle.test.ts
@@ -3,56 +3,47 @@ import { useToggle } from '../src';
 
 describe('useToggle test:', () => {
   test('should print initial value', () => {
-    const { result } = renderHook(() => useToggle());
-    const [state, setState, toggle] = result.current;
+    const { result } = renderHook(() => useToggle(['day', 'night']));
+    const [state, toggle, setState] = result.current;
 
-    expect(state).toBe(false);
-    expect(typeof setState).toBe('function');
+    expect(state).toBe('day');
     expect(typeof toggle).toBe('function');
+    expect(typeof setState).toBe('function');
   });
 
-  test('should print custom initial value', () => {
-    const { result } = renderHook(() => useToggle(true));
+  test('should use the first option as default', () => {
+    const { result } = renderHook(() => useToggle(['day', 'night']));
     const [state] = result.current;
 
-    expect(state).toBe(true);
+    expect(state).toBe('day');
+  });
+
+  test('should use the second parameter as default value', () => {
+    const { result } = renderHook(() => useToggle(['day', 'night'], 'night'));
+    const [state] = result.current;
+
+    expect(state).toBe('night');
   });
 
   test('should print the mutated value with setState function', () => {
-    const { result } = renderHook(() => useToggle());
-    const [state, setState] = result.current;
+    const { result } = renderHook(() => useToggle(['light', 'dark', 'system']));
+    const [, , setState] = result.current;
 
-    expect(state).toBe(false);
+    act(() => setState('system'));
+    expect(result.current[0]).toBe('system');
 
-    act(() => {
-      setState(true);
-    });
-
-    expect(result.current[0]).toBe(true);
-
-    act(() => {
-      setState(false);
-    });
-
-    expect(result.current[0]).toBe(false);
+    act(() => setState('dark'));
+    expect(result.current[0]).toBe('dark');
   });
 
   test('should print the mutated value with toggle function', () => {
-    const { result } = renderHook(() => useToggle(true));
-    const [state, _, toogle] = result.current;
+    const { result } = renderHook(() => useToggle(['light', 'dark', 'system']));
+    const [, toogle] = result.current;
 
-    expect(state).toBe(true);
+    act(() => toogle());
+    expect(result.current[0]).toBe('dark');
 
-    act(() => {
-      toogle();
-    });
-
-    expect(result.current[0]).toBe(false);
-
-    act(() => {
-      toogle();
-    });
-
-    expect(result.current[0]).toBe(true);
+    act(() => toogle());
+    expect(result.current[0]).toBe('system');
   });
 });
