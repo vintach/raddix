ea4a69d5a6b9f3d4896977e351505fd440dbe15b
Moisés Machuca Valverde <rmoimv@gmail.com>
2024-07-02 23:42:07 -0500

    feat: new API for useToggle hook (#96)
    The hook structure and logic has been completely modified. It no longer toggles a boolean value by default, but now requires an array of values ​​to toggle.

--- packages/hooks/use-toggle/src/index.ts
+++ packages/hooks/use-toggle/src/index.ts
@@ -1,1 +1,17 @@
-export { useToggle } from './use-toggle';
+import { useState, useCallback } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
+type ReturnValue<T> = [T, () => void, Dispatch<SetStateAction<T>>];
+
+export const useToggle = <T>(values: T[], initValue?: T): ReturnValue<T> => {
+  const [value, setValue] = useState(initValue ?? values[0]);
+
+  const toggle = useCallback(() => {
+    setValue(prev => {
+      const currentIndex = values.indexOf(prev);
+      const nextIndex = (currentIndex + 1) % values.length;
+      return values[nextIndex];
+    });
+  }, [values]);
+
+  return [value, toggle, setValue];
+};
