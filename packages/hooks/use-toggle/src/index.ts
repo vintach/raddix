4aee11ff80c7b14fa8ab10a0da6328f8ca9948a8
Moises Machuca Valverde <rmoimv@gmail.com>
2024-07-02 21:31:13 -0500

    feat: rename the parameter `options` to `values`

--- packages/hooks/use-toggle/src/index.ts
+++ packages/hooks/use-toggle/src/index.ts
@@ -2,16 +2,16 @@ import { useState, useCallback } from 'react';
 import type { Dispatch, SetStateAction } from 'react';
 type ReturnValue<T> = [T, () => void, Dispatch<SetStateAction<T>>];
 
-export const useToggle = <T>(options: T[], initValue?: T): ReturnValue<T> => {
-  const [value, setValue] = useState(initValue ?? options[0]);
+export const useToggle = <T>(values: T[], initValue?: T): ReturnValue<T> => {
+  const [value, setValue] = useState(initValue ?? values[0]);
 
   const toggle = useCallback(() => {
     setValue(prev => {
-      const currentIndex = options.indexOf(prev);
-      const nextIndex = (currentIndex + 1) % options.length;
-      return options[nextIndex];
+      const currentIndex = values.indexOf(prev);
+      const nextIndex = (currentIndex + 1) % values.length;
+      return values[nextIndex];
     });
-  }, [options]);
+  }, [values]);
 
   return [value, toggle, setValue];
 };
