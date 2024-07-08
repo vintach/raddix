e9c091e3d85e56a37685d8eeb2aec31522e0acbb
Moises Machuca Valverde <rmoimv@gmail.com>
2024-07-08 18:47:32 -0500

    feat: add useClient hook

---
+++ packages/hooks/use-client/src/index.ts
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+
+export const useClient = (): boolean => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient;
+};
